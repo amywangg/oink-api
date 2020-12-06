355c4a0d1b674119f1ca1b877c97b43f6e933741
Amy <amy_wangg@hotmail.com>
2020-12-06 18:32:20 -0500

    finalize api by going through an cleaning up bugs, migrate final db structure

--- budget.py
+++ budget.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, url_for, redirect, jsonify, Blueprint, json
 from models import Budget
 from app import SQLAlchemy
+import json
 db = SQLAlchemy()
 
 budget = Blueprint('budget', __name__)
@@ -11,7 +12,7 @@ def createBudget():
     if request.method == 'POST':
         params = request.json
         category = params['category']
-        budget = int(params['amount'])
+        budget = float(params['amount'])
         customer_id = params['client_id']
 
     try:
@@ -32,5 +33,14 @@ def getBudget(budget_id):
     try:
         budget = Budget.query.filter_by(id=budget_id).first()
         return jsonify(budget.serialize())
+    except Exception as e:
+        return(str(e))
+
+
+@budget.route("/user/<user_id>", methods=['GET'])
+def getBudgetByUser(user_id):
+    try:
+        budget = Budget.query.filter_by(customer_id=user_id).all()
+        return jsonify(Budget.serialize_list(budget))
     except Exception as e:
         return(str(e))
